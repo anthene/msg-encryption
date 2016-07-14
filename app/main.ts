23206ef3b9acd6695ac744f486602035f26b27e2
Anton <anthene@mail.ru>
2016-07-14 13:48:25 -0700

    #3: Simple encryption is used for 'text-changed' events

--- app/main.ts
+++ app/main.ts
@@ -1,1 +1,19 @@
-console.log("msg-encryption started");
+function $<T extends HTMLElement>(id: string): T {
+    return <T>document.getElementById(id);
+}
+
+function encrypt(msg: string, pswd: string): string {
+    return msg + ' ' + pswd;
+}
+
+function updateCypher(e: Event) {
+    const msg = $<HTMLInputElement>("msg").value;
+    const pswd = $<HTMLInputElement>("pswd").value;
+    $<HTMLInputElement>("cyp").value = encrypt(msg, pswd);
+}
+
+for (let id of ["msg", "pswd"]){
+    $(id).onkeyup = updateCypher;
+    $(id).onpaste = updateCypher;
+    $(id).oninput = updateCypher;
+}
