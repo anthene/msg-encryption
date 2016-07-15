ae853dd1c33359ea26923df308f06f40721b2e2b
Anton Medvedev <anton.medvedev@mandosys.com>
2016-07-15 21:38:02 +0300

    #6: Decryption page with fake decryption

--- app/main.ts
+++ app/main.ts
@@ -8,14 +8,30 @@ function encrypt(msg: string, pswd: string): string {
     return Base64.encode(msg + ' ' + pswd);
 }
 
+function decrypt(cyp: string, pswd: string): string {
+    return Base64.decode(cyp);
+}
+
 function updateCypher(e: Event) {
     const msg = $<HTMLInputElement>("msg").value;
     const pswd = $<HTMLInputElement>("pswd").value;
     $<HTMLInputElement>("cyp").value = encrypt(msg, pswd);
 }
 
+function updateMessage(e: Event) {
+    const cyp = $<HTMLInputElement>("dec-cyp").value;
+    const pswd = $<HTMLInputElement>("dec-pswd").value;
+    $<HTMLInputElement>("dec-msg").value = decrypt(cyp, pswd);
+}
+
 for (let id of ["msg", "pswd"]){
     $(id).onkeyup = updateCypher;
     $(id).onpaste = updateCypher;
     $(id).oninput = updateCypher;
+}
+
+for (let id of ["dec-cyp", "dec-pswd"]){
+    $(id).onkeyup = updateMessage;
+    $(id).onpaste = updateMessage;
+    $(id).oninput = updateMessage;
 }
