34cf9c9bcb9697bf2b92863c88957a281878983d
Anton Medvedev <anton.medvedev@mandosys.com>
2016-07-16 00:21:16 +0300

    #7: AES encryption is used. Dirty code

---
+++ my-typings/aes-js.d.ts
@@ -0,0 +1,19 @@
+declare module 'aes-js' {
+    export namespace util {
+        export function convertStringToBytes(text: string, encoding: string): number[];
+        export function convertBytesToString(bytes: number[], encoding: string): string;
+        export function decode(input: string): string;
+    }
+
+    export namespace ModeOfOperation {
+        export class ctr {
+            constructor (key: number[], counter: Counter);
+            encrypt(bytes: number[]): number[];
+            decrypt(bytes: number[]): number[];
+        }
+    }
+
+    export class Counter {
+        constructor (num: number);
+    }
+}
